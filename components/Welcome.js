7f216444fd9e33bd42e6305b828770c6d7a4d304
Robert Heler <robertheler@gmail.com>
2020-06-07 10:43:35 -0700

    Removed details screen

--- components/Welcome.js
+++ components/Welcome.js
@@ -8,22 +8,15 @@ import {StyleSheet, Image, TouchableOpacity, ActivityIndicator} from "react-nati
 import {Animated} from "react-native";
 import Home from './screens/Home.js'
 
-function DetailsScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Details!</Text>
-    </View>
-  );
-}
 
-function HomeScreen({ navigation }) {
+function FeedScreen({ navigation }) {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home screen</Text>
-      <Button
+      <Text>Feed screen</Text>
+      {/* <Button
         title="Go to Details"
         onPress={() => navigation.navigate('Details')}
-      />
+      /> */}
     </View>
   );
 }
@@ -32,35 +25,14 @@ function SettingsScreen({ navigation }) {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Settings screen</Text>
-      <Button
+      {/* <Button
         title="Go to Details"
         onPress={() => navigation.navigate('Details')}
-      />
+      /> */}
     </View>
   );
 }
 
-const HomeStack = createStackNavigator();
-
-function HomeStackScreen() {
-  return (
-    <HomeStack.Navigator>
-      <HomeStack.Screen name="Home" component={HomeScreen} />
-      <HomeStack.Screen name="Details" component={DetailsScreen} />
-    </HomeStack.Navigator>
-  );
-}
-
-const SettingsStack = createStackNavigator();
-
-function SettingsStackScreen() {
-  return (
-    <SettingsStack.Navigator>
-      <SettingsStack.Screen name="Settings" component={SettingsScreen} />
-      <SettingsStack.Screen name="Details" component={DetailsScreen} />
-    </SettingsStack.Navigator>
-  );
-}
 
 const Tab = createBottomTabNavigator();
 
@@ -68,33 +40,16 @@ export default class Welcome extends Component {
   constructor(props){
     super(props);
     this.state = {
-      fadeOutValue: new Animated.Value(1),
-      fadeInValue: new Animated.Value(0)
+
     };
   }
-
-  componentDidMount = () => {
-    this.fadeOutAndDisplayApp();
-  }
-
-  fadeOutAndDisplayApp = () => {
-    Animated.timing(this.state.fadeOutValue, {
-      toValue: 0,
-      duration: 2000
-    }).start();
-    Animated.timing(this.state.fadeInValue, {
-      toValue: 1,
-      duration: 2000
-    }).start();
-  };
-
 
   render () {
     return (
       <NavigationContainer style={{position: "absolute"}}>
         <Tab.Navigator>
-          <Tab.Screen name="Home" component={HomeStackScreen} />
-          <Tab.Screen name="Settings" component={SettingsStackScreen} />
+          <Tab.Screen name="Feed" component={FeedScreen} />
+          <Tab.Screen name="Settings" component={SettingsScreen} />
         </Tab.Navigator>
       </NavigationContainer>
     );
